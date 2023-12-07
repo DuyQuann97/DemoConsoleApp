1429a82b39afdee4ca20728d3139b986d8ef250f
DQuan97 <ngduyquan97@gmail.com>
2023-12-07 20:37:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const float PI = 3.14f;
+            Console.Write("Nhap ban kinh r = ");
+            float radius = float.Parse(Console.ReadLine());
+
+            float chuViHinhTron = (radius * 2) * PI;
+            float dienTichHinhTron = radius*radius*PI;
+
+            Console.WriteLine("Chu vi Hinh Tron voi ban kinh = {0} la {1}",radius,chuViHinhTron);
+            Console.WriteLine("Dien tich Hinh Tron voi ban kinh = {0} la {1}", radius, dienTichHinhTron);
+
+            Console.ReadKey();
+        }
+    }
+}
